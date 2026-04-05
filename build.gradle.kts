7e16ce81b86d2fdac1c69f1d55d119ccd0e5cf3c
1 <386029724@qq.com>
2026-04-05 13:22:19 +0800

    2.0.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("io.ktor.plugin") version "3.2.3"
     kotlin("plugin.serialization") version "2.2.0"
 }
-val projectVersion = "2.0.6"
+val projectVersion = "2.0.7"
 
 group = "com.donut.mixfilecli"
 version = projectVersion
@@ -56,7 +56,7 @@ ktor {
 
 dependencies {
     implementation("com.charleskorn.kaml:kaml:0.79.0")
-    implementation("com.github.InvertGeek:mixfile-core:2.0.6")
+    implementation("com.github.InvertGeek:mixfile-core:2.0.7")
     implementation("ch.qos.logback:logback-classic:1.3.15")
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
